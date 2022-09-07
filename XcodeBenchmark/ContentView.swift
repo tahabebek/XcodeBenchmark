50fa8eb166024333c389a893975123093906cb73
Taha Bebek <taha@poshmark.com>
2022-09-07 09:46:37 -0700

    Initial Commit

---
+++ XcodeBenchmark/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  XcodeBenchmark
+//
+//  Created by Taha Bebek on 9/7/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ XcodeBenchmark/XcodeBenchmarkApp.swift
@@ -0,0 +1,17 @@
+//
+//  XcodeBenchmarkApp.swift
+//  XcodeBenchmark
+//
+//  Created by Taha Bebek on 9/7/22.
+//
+
+import SwiftUI
+
+@main
+struct XcodeBenchmarkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
