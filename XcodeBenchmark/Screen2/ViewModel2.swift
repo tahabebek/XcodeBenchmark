cc188b7dc880ce07e729ee21a918093b7655d12a
Taha Bebek <taha@poshmark.com>
2022-09-07 19:09:47 -0700

    modify viewmodel2

--- XcodeBenchmark/Screen2/ViewModel2.swift
+++ XcodeBenchmark/Screen2/ViewModel2.swift
@@ -68,7 +68,7 @@ public class ViewModel2: ObservableObject {
     public init () {}
 
     public var title: String {
-        String(__tmp0 + __tmp1 + __tmp2 + __tmp3 + __tmp4 + __tmp5 + __tmp6 + __tmp7 + __tmp8 + __tmp9)
+        String(__tmp0 + __tmp1 + __tmp2 + __tmp3 + __tmp4 + __tmp5 + __tmp6 + __tmp7 + __tmp8)
     }
 }
 
